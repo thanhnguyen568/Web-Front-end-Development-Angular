31284b60c2aa58cafcf221033f4c79e053693f43
thanhnguyen568 <thanhnguyen230@gmail.com>
2023-05-15 21:23:56 +0700

    update ss2

---
+++ ss2-typescript/src/promise/main.ts
@@ -0,0 +1,1 @@
+let money:number = 20000;
---
+++ ss2-typescript/src/shape/main.ts
@@ -0,0 +1,5 @@
+import {Rectangle} from "./rectangle";
+
+const rectangle = new Rectangle(4,2,4,2);
+console.log(rectangle);
+console.log(rectangle.toString())
