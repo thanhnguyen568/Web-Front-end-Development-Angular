9e1172706ff4af692a10ce0dd9c75fac75cfba09
Thanh Nguyen <thanhnguyen230@gmail.com>
2023-06-05 22:25:26 +0700

    update

---
+++ product-be/src/app/model/product.ts
@@ -0,0 +1,14 @@
+import {Category} from './category';
+
+export interface Product {
+  productCode?: string;
+  productName?: string;
+  productPrice?: number;
+  productCreateDate?: string;
+  productWidth?: number;
+  productLength?: number;
+  productWeight?: number;
+  productHeight?: number;
+  productQuantity?: number;
+  category?: Category;
+}
